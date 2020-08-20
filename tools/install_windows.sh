0350c0761a6f9e4b16854f01d5a224a0b42e6e4c
QChris11 <46001774+QChris11@users.noreply.github.com>
2020-08-20 15:21:55 +1000

    Update install_windows.sh

--- tools/install_windows.sh
+++ tools/install_windows.sh
@@ -43,7 +43,7 @@ conda update -q conda -y;
 # provided versions
 echo "###########################################################"
 echo "create virtual env & installing packages"
-conda create -n anuga python=2.7.13 gdal=2.2.2 git nose numpy scipy netcdf4 matplotlib dill libpython msys2::m2w64-toolchain
+conda create -n anuga python gdal=2.2.2 git nose numpy scipy netcdf4 matplotlib dill libpython msys2::m2w64-toolchain cython future openmp -y
 conda activate anuga
 conda info --env
 
