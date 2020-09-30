db7fe5050e53282c3f7a8bc4423b377e7b5eb5a6
LixinHou <lixinhou5@gmail.com>
2020-09-30 20:56:36 +1000

    update the inlet boundary indices code

--- self_try/get_circumference_indices.py
+++ self_try/get_circumference_indices.py
@@ -6,8 +6,6 @@ from anuga import Region
 """
 We would use this method to gain the boundary indices
 """
-polygon1 = [ [10.0, 0.0], [11.0, 0.0], [11.0, 5.0], [10.0, 5.0] ]
-polygon2 = [ [10.0, 0.2], [11.0, 0.2], [11.0, 4.8], [10.0, 4.8] ]
 
 def get_cir (radius=None,center=None,domain=None,size=None,polygons=None):
     if polygons is not None:
@@ -23,26 +21,19 @@ def get_cir (radius=None,center=None,domain=None,size=None,polygons=None):
             opp2.region = polygon2
         else:
             opp2.region = Region(domain, poly=polygon2, expand_polygon=True)
+        #polygon cannot be region directly so I use the rate operator to work for that.
 
 
 
     if radius is not None and center is not None:
-        op1 = Rate_operator(domain,radius=radius,center = center)
-        op2 = Rate_operator(domain,radius=radius-size,center= center)
+
         region1 = Region(domain,radius = radius, center = center)
         region2 = Region(domain,radius = radius-size,center = center)
-        if isinstance(region1, Region):
-            op1.region = region1
-        else:
-            op1.region = Region(domain, poly=region1, expand_polygon=True)
-        if isinstance(region2, Region):
-            op2.region = region2
-        else:
-            op2.region = Region(domain, poly=region2, expand_polygon=True)
+
     if radius is None and center is None:
         indices = [x for x in opp1.region.indices if x not in opp2.region.indices]
     else:
-        indices = [x for x in op1.region.indices if x not in op2.region.indices]
+        indices = [x for x in region1.indices if x not in region2.indices]
 
     return indices
 
